b1ff253bc98b3350d465880999ac27c64b390177
Ruby Werman <rubywerman@Rubys-MacBook-Pro.local>
2023-10-19 11:02:31 -0500

    updated deployment and spacing

--- src/components/WorkText.js
+++ src/components/WorkText.js
@@ -8,32 +8,32 @@ const WorkText = () => {
     return (
         <div className="work-text">
             <span>I'm a software engineer at Tableau in core services on user, groups, and site management.</span>
-            <span>During my undergrad at UC Berkeley,</span>
-            <WorkExpand sentence="I participated in " trigger="internships" content={
+            <span>During my time at UC Berkeley,</span>
+            <WorkExpand sentence="I did" trigger="internships " content={
                 <p>
                     <SentenceWithLink 
-                        sentence="I worked on the Launch Control System for Artemis I at NASA, "
+                        sentence="At NASA, I added test tools for the human-rated Launch Control System. "
                         replacements={[{customWord: "NASA", url: "https://www.nasa.gov/humans-in-space/nasa-certifies-new-launch-control-system-for-artemis-i/"}]}
                     />
                     <SentenceWithLink 
-                        sentence="and helped implement IdP-initiated Single Logout for Tableau."
+                        sentence="Then at Tableau, I implemented identity provider initiated single logout."
                         replacements={[{customWord: "Tableau", url: "https://www.tableau.com/"}]}
                     />
                 </p>
                 }
             />
-            <WorkExpand sentence="and " trigger="research." content={
+            <WorkExpand sentence="and " trigger="not internships (research)." content={
                 <p>
                     <SentenceWithLink 
-                        sentence="I lead a team devoted to gamification and user experience at Public Editor, " 
+                        sentence="At Public Editor, I lead a team focused on gamification. " 
                         replacements={[{customWord: "Public Editor", url: "https://www.publiceditor.io/"}]}
                     />  
                     <SentenceWithLink 
-                        sentence="contributed to open source projects like Sci-kit Learn during the MLH Fellowship, " 
+                        sentence="During the MLH Fellowship, I contributed to open-source projects such as Sci-kit Learn. " 
                         replacements={[{customWord: "MLH Fellowship", url: "https://github.blog/2020-06-24-welcome-to-the-inaugural-class-of-mlh-fellows/"}]}
                     />
                     <SentenceWithLink
-                        sentence="and worked on research classifying cloud formations using deep learning at the Center for Data and Computing at the University of Chicago, which led to two papers."
+                        sentence="Additionally, I conducted research around classifying cloud formations using machine learning at the University of Chicago's Center for Data and Computing."
                         replacements={[
                             { customWord: "cloud formations", url: "https://datascience.uchicago.edu/research/is-climate-change-changing-clouds/" },
                             { customWord: "papers", url: "https://scholar.google.com/citations?user=3_8fKaEAAAAJ&hl" }
