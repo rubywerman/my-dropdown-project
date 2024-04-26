c803c38d60c6cf399d7640cc25823d6a236e27b8
Ruby Werman <rubywerman@Rubys-MacBook-Pro.local>
2024-04-25 22:07:02 -0700

    draft filter page

--- src/components/FiltersPage.js
+++ src/components/FiltersPage.js
@@ -6,6 +6,22 @@ function FiltersPage() {
         <div className="top-header">
         <div className="name">Filters</div>
         </div>
+        <div>
+         🏆 Top 1% of effect creators on Tiktok
+        </div>
+        <div>
+         👁️ Over 15 million impressions across Instagram, Tiktok, and Snapchat
+        </div>
+        <div>
+         🤝 Experience working with individual creators, brands, and podcasts
+        </div>
+        <div></div>
+        <div className='tiktok'>
+          <iframe height= '600px'src='https://www.tiktok.com/embed/7342975811806301482'></iframe>
+          <iframe height= '600px'src='https://www.tiktok.com/embed/7144141012577914117'></iframe>
+          <iframe height= '600px'src='https://www.tiktok.com/embed/7158892225148079365'></iframe>
+          <iframe height= '600px'src='https://www.tiktok.com/embed/7203397967687044358'></iframe>
+        </div>
       </div>
     );
   }
