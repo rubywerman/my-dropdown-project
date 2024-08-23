4adff9d36d6f9683f37900a75a82a4ed113a449c
Ruby Werman <rubywerman@Rubys-MacBook-Pro.local>
2024-08-23 15:45:03 -0700

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import './App.css';
 import FiltersPage from './components/FiltersPage';
-import Layout from './components/Layout';
 
 function App() {
   return (
-    <HashRouter>
+    <>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/filters" element={<FiltersPage />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/filters" element={<FiltersPage />} />
       </Routes>
-    </HashRouter>
+    </>
   );
 }
 
