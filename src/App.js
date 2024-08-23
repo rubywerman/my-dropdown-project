59238ea820e343703b24b31ad5640700e96c9fe6
Ruby Werman <rubywerman@Rubys-MacBook-Pro.local>
2024-08-23 15:34:09 -0700

    try hash rout

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import React from 'react';
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import './App.css';
 import FiltersPage from './components/FiltersPage';
 import Layout from './components/Layout';
 
-function RedirectToProperRoute() {
-  const location = useLocation();
-  useEffect(() => {
-    const path = window.location.search.replace('?', '');
-    if (path && path !== location.pathname) {
-      window.history.replaceState({}, '', path);
-    }
-  }, [location]);
-
-  return null;
-}
-
 function App() {
   return (
-    <BrowserRouter>
-      <RedirectToProperRoute />
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/filters" element={<FiltersPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
