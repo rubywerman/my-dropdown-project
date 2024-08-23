b7732bff226797b4e7d00571acce79be6488105d
Ruby Werman <rubywerman@Rubys-MacBook-Pro.local>
2024-08-23 15:30:05 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from './components/Home';
 import './App.css';
 import FiltersPage from './components/FiltersPage';
 import Layout from './components/Layout';
 
+function RedirectToProperRoute() {
+  const location = useLocation();
+  useEffect(() => {
+    const path = window.location.search.replace('?', '');
+    if (path && path !== location.pathname) {
+      window.history.replaceState({}, '', path);
+    }
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <RedirectToProperRoute />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/filters" index element={<FiltersPage />} />
+          <Route path="/filters" element={<FiltersPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/FiltersPage.js
+++ src/components/FiltersPage.js
@@ -10,17 +10,25 @@ function FiltersPage() {
          🏆 Top 1% of effect creators on Tiktok
         </div>
         <div>
-         👁️ Over 15 million impressions across Instagram, Tiktok, and Snapchat
+         👁️ Over 220 million impressions across Instagram, Tiktok, and Snapchat
         </div>
         <div>
          🤝 Experience working with individual creators, brands, and podcasts
         </div>
         <div></div>
         <div className='tiktok'>
-          <iframe height= '600px'src='https://www.tiktok.com/embed/7342975811806301482'></iframe>
-          <iframe height= '600px'src='https://www.tiktok.com/embed/7144141012577914117'></iframe>
-          <iframe height= '600px'src='https://www.tiktok.com/embed/7158892225148079365'></iframe>
-          <iframe height= '600px'src='https://www.tiktok.com/embed/7203397967687044358'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7379367110406966571'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7378590190643744042'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7378219610652298539'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7372600876348820779'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7405327575675096366'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7376768029847276842'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7380815227773930784'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7384170010719096094'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7378607033312054574'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7403868348171472174'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7342975811806301482'></iframe>
+          <iframe style={{height:'700px'}}src='https://www.tiktok.com/embed/7405327875257257259'></iframe>
         </div>
       </div>
     );
